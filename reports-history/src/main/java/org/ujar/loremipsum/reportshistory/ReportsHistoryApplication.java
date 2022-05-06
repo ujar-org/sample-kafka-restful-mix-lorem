07722cc406d207ddb664f8853a67b7baf2714325
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2022-05-06 18:43:03 +0300

    feat: add boilerplate code for ReportsHistoryApplication
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ reports-history/src/main/java/org/ujar/loremipsum/reportshistory/ReportsHistoryApplication.java
@@ -0,0 +1,11 @@
+package org.ujar.loremipsum.reportshistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsHistoryApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ReportsHistoryApplication.class, args);
+  }
+}
