b6a0080757094f6d827b3f43f771442959b94df7
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2022-05-07 14:04:22 +0300

    feat: configure local Kafka instance using docker-compose, define app configuration

---
+++ reports-history/src/main/java/org/ujar/loremipsum/reportshistory/config/KafkaTopics.java
@@ -0,0 +1,11 @@
+package org.ujar.loremipsum.reportshistory.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+@Data
+@ConfigurationProperties(prefix = "loremipsum.kafka.topics")
+public class KafkaTopics {
+  private String wordsProcessedTopic;
+  private int wordsProcessedPartitions;
+  private String wordsProcessedRetentionMs;
+}
--- words-statistic/src/main/java/org/ujar/loremipsum/wordsstatistic/config/ApplicationConfig.java
+++ words-statistic/src/main/java/org/ujar/loremipsum/wordsstatistic/config/ApplicationConfig.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({NetClientProperties.class})
+@EnableConfigurationProperties({NetClientProperties.class, KafkaTopics.class})
 public class ApplicationConfig {
 }
