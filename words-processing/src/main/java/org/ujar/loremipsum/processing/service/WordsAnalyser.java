4b891053df7395c0198e3b7cc2e1f25906e85726
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2022-05-08 13:24:19 +0300

    test: prepare stub for external http response

--- words-processing/src/main/java/org/ujar/loremipsum/processing/service/WordsAnalyser.java
+++ words-processing/src/main/java/org/ujar/loremipsum/processing/service/WordsAnalyser.java
@@ -1,12 +1,15 @@
 package org.ujar.loremipsum.processing.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.ujar.loremipsum.processing.model.Report;
 
 @Service
+@Slf4j
 public class WordsAnalyser {
 
   public Report analyze(String rawSource) {
+    log.info("Source: " + rawSource);
     return new Report("Lorem", (short) 1, (short) 1, (short) 1);
   }
 }
--- words-processing/src/test/java/org/ujar/loremipsum/processing/web/StatisticReportControllerTest.java
+++ words-processing/src/test/java/org/ujar/loremipsum/processing/web/StatisticReportControllerTest.java
@@ -1,15 +1,22 @@
 package org.ujar.loremipsum.processing.web;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.ujar.loremipsum.processing.TestUtils.getFileAsString;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -20,10 +27,21 @@ record StatisticReportControllerTest(MockMvc mockMvc) {
     this.mockMvc = mockMvc;
   }
 
+  @BeforeEach
+  void setUp() {
+    WireMock.reset();
+  }
+
   @Test
   void getReport_Success() throws Exception {
-    mockMvc.perform(
-            get("/v1/betvictor/text?p=1&l=short"))
+    stubFor(WireMock.get("/api/1/short").willReturn(
+            aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withBody(getFileAsString("response/1_short.html"))
+                .withHeader("Content-Type", MediaType.TEXT_PLAIN_VALUE)
+        )
+    );
+    mockMvc.perform(get("/v1/betvictor/text?p=1&l=short"))
         .andDo(print())
         .andExpect(status().isOk())
         .andExpect(content().json(
