2fefaec2cfba617cebc1d17379d8dab8911e43ee
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2022-05-17 18:07:43 +0300

    feat: configure micrometer and logbook defaults
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- words-processing/src/main/java/org/ujar/loremipsum/processing/config/ApplicationConfig.java
+++ words-processing/src/main/java/org/ujar/loremipsum/processing/config/ApplicationConfig.java
@@ -2,8 +2,18 @@ package org.ujar.loremipsum.processing.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.ujar.loremipsum.processing.config.logbook.LogbookConfiguration;
+import org.ujar.loremipsum.processing.config.logbook.LogbookJsonBodyFilter;
+import org.ujar.loremipsum.processing.config.logbook.LogbookResponseOnStatus;
 
 @Configuration
-@EnableConfigurationProperties({NetClientProperties.class, KafkaTopicsProperties.class})
+@Import(value = {
+    LogbookConfiguration.class,
+    PrometheusConfiguration.class
+})
+@EnableConfigurationProperties({ProjectInfoProperties.class, NetClientProperties.class, KafkaTopicsProperties.class})
+@LogbookResponseOnStatus
+@LogbookJsonBodyFilter
 class ApplicationConfig {
 }
