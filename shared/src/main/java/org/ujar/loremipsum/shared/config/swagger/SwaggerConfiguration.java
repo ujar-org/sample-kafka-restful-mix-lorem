ae89efac2a7096515f439d4eb755846f425966cb
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2022-07-17 11:16:19 +0300

    chore: add custom project info properties
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ shared/src/main/java/org/ujar/loremipsum/shared/config/swagger/SwaggerConfiguration.java
@@ -0,0 +1,47 @@
+package org.ujar.loremipsum.shared.config.swagger;
+
+import static java.util.Objects.requireNonNullElse;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.ujar.loremipsum.shared.config.ProjectInfoProperties;
+
+@Configuration
+@ConditionalOnMissingBean(OpenAPI.class)
+@Import(value = {
+    SwaggerConfiguration.K8sSwaggerConfiguration.class,
+})
+@EnableConfigurationProperties(ProjectInfoProperties.class)
+@Slf4j
+public class SwaggerConfiguration {
+
+  @Configuration
+  public static class K8sSwaggerConfiguration {
+    @Bean
+    public OpenAPI openApi(
+        @Value("${spring.application.name}") String applicationName,
+        ProjectInfoProperties projectInfoProperties
+    ) {
+      var api = new OpenAPI().info(
+          new Info()
+              .title(applicationName)
+              .version(projectInfoProperties.getVersion())
+              .description(projectInfoProperties.getDescription())
+      );
+      var server = new Server();
+      log.info("Project info properties: {}", projectInfoProperties);
+      server.setUrl(requireNonNullElse(projectInfoProperties.getRelativePath(), ""));
+      api.setServers(List.of(server));
+      return api;
+    }
+  }
+}
