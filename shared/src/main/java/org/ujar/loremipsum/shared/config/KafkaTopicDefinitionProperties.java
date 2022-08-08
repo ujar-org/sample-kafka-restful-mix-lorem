4d826ec291bad820da7ec38c232fb7f6936c972e
Dima Denysenko <coding@dimdnk.com>
2022-08-08 19:17:03 +0300

    feat: use records for props

--- shared/src/main/java/org/ujar/loremipsum/shared/config/KafkaTopicDefinitionProperties.java
+++ shared/src/main/java/org/ujar/loremipsum/shared/config/KafkaTopicDefinitionProperties.java
@@ -2,23 +2,23 @@ package org.ujar.loremipsum.shared.config;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Map;
-import lombok.Value;
+import lombok.NonNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 import org.ujar.loremipsum.shared.config.topic.TopicDefinition;
 
-@Value
 @ConstructorBinding
 @ConfigurationProperties(prefix = "loremipsum.kafka")
 @Validated
 @SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
-public class KafkaTopicDefinitionProperties {
+public record KafkaTopicDefinitionProperties (@NonNull Map<String, TopicDefinition> topics) {
 
-  Map<String, TopicDefinition> topics;
+  public Map<String, TopicDefinition> getTopics() {
+    return topics();
+  }
 
   public TopicDefinition get(String key) {
     return topics.get(key);
   }
 }
-
