9e3980fb34740568267af5ae087cbe9b01cce30f
Dima Denysenko <coding@dimdnk.com>
2023-05-11 16:10:31 +0300

    feat: enable BufferingApplicationStartup

--- words-processing/src/main/java/org/ujar/lorem/processing/WordsProcessingApplication.java
+++ words-processing/src/main/java/org/ujar/lorem/processing/WordsProcessingApplication.java
@@ -2,10 +2,13 @@ package org.ujar.lorem.processing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class WordsProcessingApplication {
   public static void main(String[] args) {
-    SpringApplication.run(WordsProcessingApplication.class, args);
+    SpringApplication springApplication = new SpringApplication(WordsProcessingApplication.class);
+    springApplication.setApplicationStartup(new BufferingApplicationStartup(2048));
+    springApplication.run(args);
   }
 }
