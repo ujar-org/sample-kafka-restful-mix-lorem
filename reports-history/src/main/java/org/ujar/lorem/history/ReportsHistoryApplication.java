f0d4793260b7c87c95ee2218c1586af5c45fcdf3
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2023-06-03 18:35:20 +0300

    chore: enable reasonable defaults for observability components

--- reports-history/src/main/java/org/ujar/lorem/history/ReportsHistoryApplication.java
+++ reports-history/src/main/java/org/ujar/lorem/history/ReportsHistoryApplication.java
@@ -2,10 +2,13 @@ package org.ujar.lorem.history;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class ReportsHistoryApplication {
   public static void main(String[] args) {
-    SpringApplication.run(ReportsHistoryApplication.class, args);
+    SpringApplication springApplication = new SpringApplication(ReportsHistoryApplication.class);
+    springApplication.setApplicationStartup(new BufferingApplicationStartup(2048));
+    springApplication.run(args);
   }
 }
--- words-processing/src/main/java/org/ujar/lorem/processing/config/ApplicationConfig.java
+++ words-processing/src/main/java/org/ujar/lorem/processing/config/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package org.ujar.lorem.processing.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +10,9 @@ import org.ujar.lorem.shared.config.BuildInfoConfig;
 import org.ujar.lorem.shared.config.KafkaErrorHandlingProperties;
 import org.ujar.lorem.shared.config.KafkaHealthIndicatorConfig;
 import org.ujar.lorem.shared.config.KafkaTopicDefinitionProperties;
-import org.ujar.lorem.shared.config.SwaggerConfig;
 
 @Configuration
 @Import(value = {
-    SwaggerConfig.class,
     BuildInfoConfig.class,
     KafkaHealthIndicatorConfig.class
 })
@@ -22,5 +22,6 @@ import org.ujar.lorem.shared.config.SwaggerConfig;
     KafkaTopicDefinitionProperties.class
 })
 @ComponentScan({"org.ujar.lorem.shared.*", "org.ujar.lorem.processing.*"})
+@OpenAPIDefinition(info = @Info(title = "Words Counter", version = "23.0.0"))
 class ApplicationConfig {
 }
